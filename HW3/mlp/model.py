dd76782c1f04c71bed94161e41cbab682c4ce000
fgj <fgj15@mails.tsinghua.edu.cn>
2018-10-24 00:15:18 +0800

    mlp v0.3

--- HW3/mlp/model.py
+++ HW3/mlp/model.py
@@ -3,13 +3,14 @@
 import tensorflow as tf
 FLAGS = tf.app.flags.FLAGS
 
+
 class Model:
     def __init__(self,
                  learning_rate=0.001,
                  learning_rate_decay_factor=0.9995):
         self.x_ = tf.placeholder(tf.float32, [None, 28*28])
         self.y_ = tf.placeholder(tf.int32, [None])
-
+        self.h_units=300
 		# TODO:  fill the blank of the arguments
         self.loss, self.pred, self.acc = self.forward()
         self.loss_val, self.pred_val, self.acc_val = self.forward()
@@ -37,6 +38,20 @@ class Model:
             # Your Relu Layer
             # Your Dropout Layer: use dropout_layer function
             # Your Linear Layer
+            x_=tf.placeholder(tf.float32,[None,784],name="x_")
+            y_=tf.placeholder(tf.float32,[None,10],name="y_")
+            W1=tf.Variable(tf.truncated_normal([784,self.h_units]))
+            b1=tf.Variable(tf.zeros([self.h_units]))
+            W2=tf.Variable(tf.truncated_normal(self.h_units,10))
+            b2=tf.Variable(tf.zeros([10]))
+
+            u1=tf.matmul(x_,W1)+b1
+            mu1,sigma1=batch_normalization_layer(u1)
+
+
+            keep_prob=tf.placeholder(tf.float32)
+
+
             logits = tf.Variable(tf.constant(0.0, shape=[100, 10]))  # deleted this line after you implement above layers
 
         loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.y_, logits=logits))
